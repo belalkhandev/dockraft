68559200316f9bb6d22126a21add5a485e6da411
Belal khan <belalkhan.dev@gmail.com>
2024-01-07 20:30:08 +0600

    initial project setup

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -6,22 +6,7 @@ import moment from "moment";
 import PaymentStatusLabel from "@/Components/PaymentStatusLabel.vue";
 
 const props = defineProps({
-    running_count: {
-        type: Number,
-        default: 0
-    },
-    idle_count: {
-        type: Number,
-        default: 0
-    },
-    repairable_count: {
-        type: Number,
-        default: 0
-    },
-    disable_count: {
-        type: Number,
-        default: 0
-    }
+
 });
 
 </script>
@@ -29,53 +14,7 @@ const props = defineProps({
 <template>
     <AdminPanelLayout title="Dashboard">
         <template #header>Dashboard</template>
-        <div class="row">
-            <div class="col-md-3 col-sm-6">
-                <div class="dashboard-card card-2">
-                    <i class='bx bx-radio-circle-marked'></i>
-                    <div class="card-header">
-                        <h5>Running</h5>
-                    </div>
-                    <div class="card-content">
-                        <h2>{{ running_count }}</h2>
 
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="dashboard-card card-3">
-                    <i class='bx bx-radio-circle-marked'></i>
-                    <div class="card-header">
-                        <h5>Idle</h5>
-                    </div>
-                    <div class="card-content">
-                        <h2>{{ idle_count }}</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="dashboard-card card-1">
-                    <i class='bx bx-radio-circle-marked'></i>
-                    <div class="card-header">
-                        <h5>Repairable</h5>
-                    </div>
-                    <div class="card-content">
-                        <h2>{{ repairable_count }}</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <div class="dashboard-card card-4">
-                    <i class='bx bx-radio-circle-marked'></i>
-                    <div class="card-header">
-                        <h5>Disable</h5>
-                    </div>
-                    <div class="card-content">
-                        <h2>{{ disable_count }}</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
 
     </AdminPanelLayout>
 </template>
